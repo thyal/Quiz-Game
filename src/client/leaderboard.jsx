8390979ff1b8456563c715e011ec382ce2d77c99
thyal <thomasdfrivold@gmail.com>
2018-11-01 15:57:41 +0100

    leaderboard component

---
+++ src/client/leaderboard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export class Leaderboard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+
+        };
+    }
+
+    render() {
+        return(
+            <div><h3>Leaderboard</h3></div>
+        );
+    }
+}
