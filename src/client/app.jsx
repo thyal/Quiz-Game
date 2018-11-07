02a37d9fd57405ddaf7e5976f0c817e071824128
thyal <thomasdfrivold@gmail.com>
2018-11-07 01:38:34 +0100

    refactoring

---
+++ src/client/app.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+import Navigation from "./components/navigation";
+import {Home} from "./components/home";
+import Leaderboard from "./components/leaderboard";
+import {NotFound} from "./components/notFound";
+import {Login} from "./components/login";
+import {Signup} from "./components/signup";
+import Profile from "./components/profile";
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <BrowserRouter>
+                <div>
+                    <Navigation />
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route exact path="/leaderboard" component={Leaderboard}/>
+                        <Route exact path="/login" component={Login}/>
+                        <Route exact path="/signup" component={Signup}/>
+                        <Route exact path="/profile" component={Profile}/>
+                        <Route component={NotFound}/>
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        );
+    }
+}
+
+export default App;
--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,43 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import {createStore} from "redux";
 import {Provider} from "react-redux";
 import rootReducer from "./reducers/rootReducer";
 
-import Navigation from "./navigation";
-import {Home} from "./home";
-import Leaderboard from "./leaderboard";
-import {NotFound} from "./notFound";
-import {Login} from "./login";
-import {Signup} from "./signup";
-import Profile from "./profile";
+import App from "./app";
 
 //REDUX
 const store = createStore(rootReducer);
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-    }
 
-    render() {
-        return(
-            <BrowserRouter>
-                <div>
-                    <Navigation />
-                    <Switch>
-                        <Route exact path="/" component={Home}/>
-                        <Route exact path="/leaderboard" component={Leaderboard}/>
-                        <Route exact path="/login" component={Login}/>
-                        <Route exact path="/signup" component={Signup}/>
-                        <Route exact path="/profile" component={Profile}/>
-                        <Route component={NotFound}/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        );
-    }
-}
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+    <App />
+    </Provider>, 
+    document.getElementById("root")
+);
