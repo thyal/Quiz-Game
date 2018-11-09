c6b974c1fd8ce7d55bb33ddf448399d5239331fe
thyal <thomasdfrivold@gmail.com>
2018-11-09 22:22:24 +0100

    comments

--- src/server/sockets/socket_handler.js
+++ src/server/sockets/socket_handler.js
@@ -5,7 +5,7 @@ const Games = require('../models/games');
 let io;
 
 const start = (server) => {
-
+    
 
     io = socketIo(server);
 
@@ -14,22 +14,19 @@ const start = (server) => {
         //We expect each socket to send the gameId of the game they want to join when they connect.
         socket.on('game', async (payload) => {
             
+            //The user joins a room (a game). So we have an unique room for each game. 
             socket.join(payload.gameId);
-            let joined;
-            try {
-                joined = await Games.joinGame(payload.user_id, payload.gameId, 0);
-            } catch(error) {
-                console.log(error);
-            }
-            
-            // if(!joined) {
 
-            // }
-            
+            //We also save each user to a table consisting of the game id, user id, and scores.
+            //We use this table both to keep track of which players is in which game, and their scores.
+            try {
+                await Games.joinGame(payload.user_id, payload.gameId, 0);
+            } catch(error) {
+                socket.to(payload.gameId).emit('error', 'Something went wrong');
+            }
             socket.to(payload.gameId).emit('newUser', payload);
         });
     });
 };
-
 
 module.exports = {start};
